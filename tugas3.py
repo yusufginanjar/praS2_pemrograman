0c06c2228aeeca552a8fe1b5ab568eb02bd39b5f
yusufginanjar <yusufginanjar16@gmail.com>
2022-10-28 22:32:01 +0700

    pemrograman: tugas 3

--- tugas3.py
+++ tugas3.py
@@ -1,4 +1,5 @@
-# create a function that print * in triangle
+# Buatlah sebuah fungsi bernama segitiga yang menerima masukan sebuah
+# bilangan bulat dan mengeluarkan luaran sebagai berikut (bentuk segitiga dari *)
 def segitiga(n):
     for i in range(1, n+1):
         print('*'*i)
@@ -7,3 +8,27 @@ def segitiga(n):
 #execute the function
 segitiga(3)
 segitiga(4)
+
+
+# in one class, there are 7 students named Andi, Budi, Cepi, Dodi, Enggar, Fino and Geri
+andi = {'name': 'Andi'}
+budi = {'name': 'Budi', 'extra': 'Pramuka'}
+cepi = {'name': 'Cepi', 'extra': 'Pramuka', 'suka': 'Geografi'}
+dodi = {'name': 'Dodi'}
+enggar = {'name': 'Enggar', 'extra': 'Pramuka'}
+fino = {'name': 'Fino' , 'suka': 'Geografi'}
+geri = {'name': 'Geri' , 'suka': 'Geografi'}
+
+
+students = [andi, budi, cepi, dodi, enggar, fino, geri]
+print(students)
+
+print('Siswa yang tidak mengikuti Extrakulikuler:')
+for student in students:
+    if 'extra' not in student:
+        print(student['name'])
+
+print('Siswa yang mengikuti Pramuka dan suka Geografi:')
+for student in students:
+    if 'extra' in student and student['extra'] == 'Pramuka' and 'suka' in student and student['suka'] == 'Geografi':
+        print(student['name'])
